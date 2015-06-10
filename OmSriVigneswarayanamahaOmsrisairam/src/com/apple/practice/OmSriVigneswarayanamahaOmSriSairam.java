da5d10b9025edc13a8c6a33026edc9d07161eee1
somasundar-philips <somasundar.m@gmail.com>
2015-06-10 15:01:09 -0700

    My first changes done to the file. Added new line.

--- OmSriVigneswarayanamahaOmsrisairam/src/com/apple/practice/OmSriVigneswarayanamahaOmSriSairam.java
+++ OmSriVigneswarayanamahaOmsrisairam/src/com/apple/practice/OmSriVigneswarayanamahaOmSriSairam.java
@@ -5,6 +5,7 @@ public class OmSriVigneswarayanamahaOmSriSairam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Om Sri Vigneswaraya namaha.... Om Sri Sairam... Om Sri Venkateswaraya namaha... Om sri durga deviyai namaha... Om Sri lakshmi deviyai namaha...s");
+		System.out.println(" Now added new changes to the file");
 	}
 
 }
