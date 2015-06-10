7fe17cb960c5271e4aff35b050f5d2a82f7ebf02
somasundar-philips <somasundar.m@gmail.com>
2015-06-10 14:55:20 -0700

    My first git commit

---
+++ OmSriVigneswarayanamahaOmsrisairam/src/com/apple/practice/OmSriVigneswarayanamahaOmSriSairam.java
@@ -0,0 +1,10 @@
+package com.apple.practice;
+
+public class OmSriVigneswarayanamahaOmSriSairam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Om Sri Vigneswaraya namaha.... Om Sri Sairam... Om Sri Venkateswaraya namaha... Om sri durga deviyai namaha... Om Sri lakshmi deviyai namaha...s");
+	}
+
+}
